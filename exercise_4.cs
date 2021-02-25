c8f926010923e89e7526c05c63dea595181077c1
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-02-25 21:34:53 +0200

    Create exercise_4.cs
                 readline 2 numbers from the user
                 create a task that computes the sum of this two numbers
                 have the task use a return value 
                 use Task.FromResult
                  print the result

---
+++ exercise_4.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Taks24._02._2021
+{
+    class Program
+    {
+        static int[] numbers = new int[10];
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter number #1: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter number #2: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Task<int> t =  Task.FromResult(number1 + number2);
+
+            Console.WriteLine($"{number1} + {number2} = {t.Result}");
+        }
+    }
+}
