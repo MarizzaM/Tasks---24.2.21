1c4a138ef1a87c2cd835a5ecf943539cd132a89a
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-02-25 20:47:07 +0200

    Update exercise_1.cs
       use Stopwatch
       create 10 threads that print numbers from 1 to 10 , and run them
       how long did it take?
       create 10 Tasks that print numbers from 1 to 10 , and run them
       how long did it take?

--- exercise_1.cs
+++ exercise_1.cs
@@ -12,54 +12,51 @@ namespace Taks24._02._2021
     {
         static int[] numbers = new int[10];
         static void Main(string[] args)
-        {
-            Stopwatch stopwatch = new Stopwatch();
+        { 
+            //10 THREADS
 
+            List<Thread> ten_treads = new List<Thread>();
+
+            Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
-            ParameterizedThreadStart index = new ParameterizedThreadStart(Set10ItemsInArray);
-            List<Thread> threadsTo10 = new List<Thread>();
-            for (int i = 0; i < 10; i++)
-            {
-                var t = new Thread(index);
-                threadsTo10.Add(t);
-                Console.WriteLine($"Thread #{i+1}");
+            Thread t = new Thread(() => {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"Thread #{i}");
+                }
+            });
+
+            t.Start();
+            ten_treads.Add(t);
+
+            foreach (var item in ten_treads) {
+                item.Join();
             }
+            Console.WriteLine("The total time elapsed using 10 threads is: {0} ms\n", stopwatch.Elapsed.TotalMilliseconds);
             stopwatch.Stop();
 
+            // 10 TASKS
 
+            stopwatch.Reset();
+            stopwatch.Start();
 
-            Stopwatch stopwatch1 = new Stopwatch();
+            var ten_tasks = new Task[10];
 
-            stopwatch1.Start();
-
-            Console.WriteLine("Time elapsed: {0}", stopwatch1.Elapsed);
-
-
-            var tasks = new Task[10];
-            //AutoResetEvent are = new AutoResetEvent(true);
-            for (int i = 0; i < tasks.Length; i++)
+            for (int i = 0; i < ten_tasks.Length; i++)
             {
-                tasks[i] = Task.Run(() =>
+                ten_tasks[i] = Task.Run(() =>
                 {
-                        Console.WriteLine($"Task #{Task.CurrentId}");
+                    Console.WriteLine($"Task #{Task.CurrentId}");
                 });
             }
 
-            for (int i = 0; i < tasks.Length; i++)
+            for (int i = 0; i < ten_tasks.Length; i++)
             {
-                Task.WaitAll(tasks);
+                Task.WaitAll(ten_tasks);
             }
-            stopwatch1.Stop();
-
-        }
-        static void Set10ItemsInArray(object i)
-        {
-            for (int index = (int)i; index < ((int)i + 1); index++)
-            {
-                numbers[index] = 1;
-            }
+            Console.WriteLine("The total time elapsed using 10 tasks is: {0} ms", stopwatch.Elapsed.TotalMilliseconds);
+            stopwatch.Stop();
         }
     }
 }
